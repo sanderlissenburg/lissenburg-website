33adae4d35615677fd9d0d11efdac97b263f9320
Sander Lissenburg <sander.lissenburg@bitfactory.nl>
2020-04-25 21:38:45 +0200

    traefik and lets encrypt

--- deploy/vps/deploy.sh
+++ deploy/vps/deploy.sh
@@ -8,4 +8,5 @@ docker push lissenburg/lissenburg-client
 # Deploy to vps
 ssh $SSH_USER@$SSH_HOST 'mkdir -p ~/www/lissenburg/'
 scp -r ./deploy/vps/* $SSH_USER@$SSH_HOST:~/www/lissenburg/
+ssh $SSH_USER@$SSH_HOST 'cd ~/www/lissenburg/ && touch acme.json && chmod 600 acme.json'
 ssh $SSH_USER@$SSH_HOST 'cd ~/www/lissenburg/ && docker-compose pull && docker stack deploy -c <(docker-compose config) lissenburg'
