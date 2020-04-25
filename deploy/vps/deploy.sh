df1c5c1c61c447d727866859288edf3ad5c1dee0
Sander Lissenburg <sander.lissenburg@bitfactory.nl>
2020-04-25 14:54:55 +0200

    Fix for final deployment step

--- deploy/vps/deploy.sh
+++ deploy/vps/deploy.sh
@@ -8,4 +8,4 @@ docker push lissenburg/lissenburg-client
 # Deploy to vps
 ssh -i ./deploy/travis/travis_deploy_key $(SSH_USER)@$(SSH_HOST) 'mkdir -p ~/www/lissenburg/'
 scp -r ./deploy/vps/* $(SSH_USER)@$(SSH_HOST):~/www/lissenburg/
-ssh -i ./travis/travis_deploy_key $(SSH_USER)@$(SSH_HOST) 'cd ~/www/lissenburg/ && docker-compose pull && docker stack deploy -c <(docker-compose config) lissenburg'
+ssh -i ./deploy/travis/travis_deploy_key $(SSH_USER)@$(SSH_HOST) 'cd ~/www/lissenburg/ && docker-compose pull && docker stack deploy -c <(docker-compose config) lissenburg'
